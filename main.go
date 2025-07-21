42f5e420f09871dcba3fcd4c62a21dcf30578501
tdpprog <arsenijpsonko4842@gmail.com>
2025-07-21 23:49:20 +0500

    ыфвафыва фывафывавыфа

--- main.go
+++ main.go
@@ -275,9 +275,9 @@ func updateTelegramMessage(ctx context.Context) {
 func generateMessageText() string {
 	today := time.Now().In(loc).Format("2006-01-02")
 	return fmt.Sprintf("🗡 Статистика за %s:\n\n"+
-		"5nm: %d/%d\n5: %d/%d \n"+
-		"6: %d/%d\n+7nm: %d/%d\n"+
-		"7: %d/%d\nMEGA: %d/%d\n\n"+
+		"5nm: %d/%d\n5:    %d/%d\n"+
+		"6:   %d/%d\n+7nm: %d/%d\n"+
+		"7:   %d/%d\nMEGA: %d/%d\n\n"+
 		"Ботинки: %d/%d\nШлем: %d/%d\n"+
 		"Нагрудник: %d/%d\nШтаны: %d/%d",
 		today,
